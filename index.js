0e7ac32374b37d0690eb40c7d179ccb734405e09
Risto Stevcev <risto1@gmail.com>
2019-03-11 14:31:53 +0100

    Add array check for sources property

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const globby = require('globby')
 const flatten = require('arr-flatten')
 
 module.exports = bsconfig => {
-  return Promise.all(bsconfig.sources.map(source =>
+  const sources = bsconfig.sources instanceof Array ? bsconfig.sources : [bsconfig.sources]
+  return Promise.all(sources.map(source =>
       source.files ?
         Promise.resolve(source.files.map(file => path.join(process.cwd(), source.dir, file))) :
         globby([path.join(process.cwd(), source.dir, source.subdirs ? '**' : '', '*.ml')])
